6db47e35ae590b059fe508b49e8521bd233d5ab2
Flavio <flaver.vr@gmail.com>
2019-12-14 13:26:22 -0600

    variable

--- bash/terraform.sh
+++ bash/terraform.sh
@@ -1,5 +1,6 @@
 
-export TF_VAR_image_id=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" "https://api.digitalocean.com/v2/images?private=true" |jq ."images[] | select(.name == \"tec-app-$ARTIFACT_ID\") | .id")
+export TF_VAR_image_id=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" "https://api.digitalocean.com/v2/images?private=true" | jq ."images[] | select(.name == \"tec-app-$ARTIFACT_ID\") | .id")
+echo $TF_VAR_image_id
 cd terraform
 terraform init
 terraform apply -input=false -auto-approve
