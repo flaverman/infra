4dc3908661093c90c81c1b4674566a9abe8cd77f
Flavio <flaver.vr@gmail.com>
2019-12-14 13:39:02 -0600

    1

--- bash/terraform.sh
+++ bash/terraform.sh
@@ -1,4 +1,5 @@
-
+echo $DIGITALOCEAN_TOKEN
+echo $ARTIFACT_ID
 export TF_VAR_image_id=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_TOKEN" "https://api.digitalocean.com/v2/images?private=true" | jq ."images[] | select(.name == \"tec-app-$ARTIFACT_ID\") | .id")
 echo $TF_VAR_image_id
 cd terraform
